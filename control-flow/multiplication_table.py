e863ac349ae5aedf8288193aab5d751e8c5fa7a8
Jason <jasonretro580@gmail.com>
2025-05-28 19:10:01 +0300

    removed the third line and replaced

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,3 @@
 number = int(input("Enter a number to see its multiplication table: "))
 for i in range (1, 11):  # Starts at 1 and stops at 10
-    result = number * i
-    print(f"{number} x {i} = ", result)
+    print(f"{number} * {i} = {number * i}")
