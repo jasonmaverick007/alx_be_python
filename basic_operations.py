0528742865e91588318d474484dd37e109e156d2
Jason <jasonretro580@gmail.com>
2025-05-28 13:34:20 +0300

    basic_operations python file

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result = number1 - number2
+product = number1 * number2
+sum = number1 + number2
+print("Additon of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is", result)
+print("Multiplication of 10 and 5 is", product)
