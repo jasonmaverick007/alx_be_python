0cd1d8af3374017e7aa15156bbfecb69ab1b62dc
Jason <jasonretro580@gmail.com>
2025-05-28 17:03:27 +0300

    changed else to elif

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,5 +3,5 @@ if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-else:
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
