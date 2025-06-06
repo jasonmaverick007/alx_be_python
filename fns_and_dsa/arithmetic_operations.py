c73349b6af377041de3c671d61019e01f6802848
Jason <jasonretro580@gmail.com>
2025-06-06 13:52:45 +0300

    Added dividing by zero scenario in arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,6 +6,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
+        if num2 == 0:
+            return "Error: dividing by zero" 
         return num1 / num2
     else:
         return "Error: Invalid operation"
