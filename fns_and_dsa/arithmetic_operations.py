3fdc75252ae3e015d334bd2b0e2297d2d03864d8
Jason <jasonretro580@gmail.com>
2025-06-06 13:28:23 +0300

    Added arithmetic_operations.py and main.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
