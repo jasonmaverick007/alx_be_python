6afe669f1fa8db7da5c065ec24879ae16523d8f5
Jason <jasonretro580@gmail.com>
2025-06-11 14:12:05 +0300

    Add robust_division_calculator.py and main.py

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+def safe_divide(numerator, denominator ):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+
+        if den == 0:
+            return "Error: Cannot divide by zero."
+        
+        result = num / den
+        return f"{result}"
+    
+    except ValueError:
+        return "Error: Please enter numeric values only."
